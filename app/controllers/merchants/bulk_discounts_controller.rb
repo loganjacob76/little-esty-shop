4ac639e16ac61155da4aaa5bb0561aa62f2aa6cc
loganjacob76 <nagolisback@gmail.com>
2021-04-27 16:09:22 -0600

    Set up controller and view

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -9,7 +9,23 @@ class Merchants::BulkDiscountsController < ApplicationController
   end
 
   def new
-    binding.pry
     @discount = BulkDiscount.new
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.bulk_discounts.new(bulk_discount_params)
+
+    if discount.save
+      redirect_to merchant_bulk_discounts_path
+    else
+      redirect_to new_merchant_bulk_discount_path
+      flash[:error] = "Error: #{error_message(discount.errors)}"
+    end
+  end
+
+  private
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:name, :percent_discount, :quantity_threshold)
   end
 end
