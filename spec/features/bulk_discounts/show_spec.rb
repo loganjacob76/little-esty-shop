716573fbd52bbe5e8bad0ed0a586dc086dbde279
loganjacob76 <nagolisback@gmail.com>
2021-04-27 16:53:57 -0600

    Update test for show pg

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -6,13 +6,22 @@ RSpec.describe 'bulk discount show page' do
     @discount = create(:bulk_discount, merchant: @merchant)
   end
 
-  describe 'you are on the discounts index page' do
+  context 'you are on the discounts index page' do
     it 'has a link to take you to the specific discount show page' do
       visit merchant_bulk_discounts_path(@merchant)
 
       click_on @discount.name
 
       expect(current_path).to eq(merchant_bulk_discount_path(@merchant, @discount))
+    end
+  end
+
+  context 'you are on the discount show page' do
+    it 'has the quantity threshold and percent discount' do
+      visit merchant_bulk_discount_path(@merchant, @discount)
+      
+      expect(page).to have_content(@discount.quantity_threshold)
+      expect(page).to have_content(@discount.percent_discount)
     end
   end
 end
